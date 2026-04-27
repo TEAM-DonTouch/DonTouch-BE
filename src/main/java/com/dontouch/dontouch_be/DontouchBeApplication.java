de7143895717a3dd03245327b09d89096dadc400
kjh0718 <ranbow3062961@gmail.com>
2026-04-28 00:09:38 +0900

    project init

---
+++ src/main/java/com/dontouch/dontouch_be/DontouchBeApplication.java
@@ -0,0 +1,13 @@
+package com.dontouch.dontouch_be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DontouchBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DontouchBeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dontouch/dontouch_be/DontouchBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dontouch.dontouch_be;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DontouchBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

4e344a11ff64acc15c4391f337e090d068831ab8
강지윤 <23011595@sju.ac.kr>
2026-06-30 01:37:20 +0900

    로그인_회원가입_기능추가 : fix : Redis의 원자 연산으로 refresh token rotation 원자 처리 https://github.com/TEAM-DonTouch/DonTouch-BE/issues/18

--- src/main/java/shop/dontouch/dontouch_be/domain/auth/repository/RefreshTokenRepository.java
+++
@@ -1,8 +0,0 @@
-package shop.dontouch.dontouch_be.domain.auth.repository;
-
-import org.springframework.data.repository.CrudRepository;
-import shop.dontouch.dontouch_be.domain.auth.entity.RefreshToken;
-
-public interface RefreshTokenRepository extends CrudRepository<RefreshToken, String> {
-
-}

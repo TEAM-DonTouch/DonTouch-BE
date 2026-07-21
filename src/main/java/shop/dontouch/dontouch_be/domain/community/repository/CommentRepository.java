e5a7bbc54b992cc673f3381b689fef2ba05db895
kjh0718 <ranbow3062961@gmail.com>
2026-07-21 22:35:39 +0900

    댓글_API_정리 : perf : 댓글 목록 조회 N+1 방지 (EntityGraph 적용)

--- src/main/java/shop/dontouch/dontouch_be/domain/community/repository/CommentRepository.java
+++ src/main/java/shop/dontouch/dontouch_be/domain/community/repository/CommentRepository.java
@@ -2,11 +2,14 @@ package shop.dontouch.dontouch_be.domain.community.repository;
 
 import java.util.UUID;
 import org.springframework.data.domain.Page;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import shop.dontouch.dontouch_be.domain.community.entity.Comment;
 import org.springframework.data.domain.Pageable;
 
 public interface CommentRepository extends JpaRepository<Comment, UUID> {
 
+  @EntityGraph(attributePaths = "user")
   Page<Comment> findAllByPostIdOrderByCreatedAtAsc(UUID postId, Pageable pageable);
+
 }

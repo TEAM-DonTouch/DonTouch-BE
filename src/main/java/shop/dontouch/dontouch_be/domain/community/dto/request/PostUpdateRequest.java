bf203d86f48f7391b1b20c8ee5ca7e83f73a7f30
강지윤 <23011595@sju.ac.kr>
2026-08-06 04:41:52 +0900

    예산_대비_남은_금액_계산_및_반영 : fix : 게시글 수정 시 여러 줄 본문 입력 허용 https://github.com/TEAM-DonTouch/DonTouch-BE/issues/31

--- src/main/java/shop/dontouch/dontouch_be/domain/community/dto/request/PostUpdateRequest.java
+++ src/main/java/shop/dontouch/dontouch_be/domain/community/dto/request/PostUpdateRequest.java
@@ -17,6 +17,6 @@ public class PostUpdateRequest {
 
   @Schema(description = "게시글 내용", example = "이번 달에 식비를 20% 줄였어요.")
   @Size(max = 255, message = "내용은 255자를 초과할 수 없습니다.")
-  @Pattern(regexp = "^(?!\\s*$).*$", message = "내용은 공백만 입력할 수 없습니다.")
+  @Pattern(regexp = "^(?!\\s*$)[\\s\\S]*$", message = "내용은 공백만 입력할 수 없습니다.")
   private String content;
 }
